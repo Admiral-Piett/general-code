b483abc3917c3e0f5c5c40c439f1a4b895888310
Devin Humphreys <humphred@advisory.com>
2018-01-17 14:32:10 -0500

    Helpers 1/17/2018

---
+++ devin_helpers.py
@@ -0,0 +1,26 @@
+from tabulate import tabulate as tb
+
+#Pretty prints dataframes for me in an easier way than typing the full command each time
+def dfpp(df, head=''):
+    if head == 'yes':
+        print tb(df.head(), headers='keys')
+    else:
+        print tb(df, headers='keys')
+        
+#Converts lists to strings
+def convertToString(value):
+    strValue = ''
+    for v in value:
+        strValue = strValue + str(v) + ','
+    strValue = strValue[:-1]
+
+    return strValue
+
+
+def columnCount(dataframe):
+    headerList = dataframe.columns
+    count = -1
+
+    for v in headerList:
+        count = count + 1
+        print str(v) + " - " + str(count)
