1a8a108b716437d2c6b287fdc5d07360a6e64d7f
Devin Humphreys <humphred@advisory.com>
2018-01-30 11:00:07 -0500

    Update 1/30/2018

--- devin_helpers.py
+++ devin_helpers.py
@@ -1,13 +1,16 @@
 from tabulate import tabulate as tb
+import json
 
-#Pretty prints dataframes for me in an easier way than typing the full command each time
+
+# Pretty prints dataframes for me in an easier way than typing the full command each time
 def dfpp(df, head=''):
     if head == 'yes':
         print tb(df.head(), headers='keys')
     else:
         print tb(df, headers='keys')
-        
-#Converts lists to strings
+
+
+# Converts lists to strings
 def convertToString(value):
     strValue = ''
     for v in value:
@@ -24,3 +27,7 @@ def columnCount(dataframe):
     for v in headerList:
         count = count + 1
         print str(v) + " - " + str(count)
+
+
+def jsonpp(js):
+    print json.dumps(js, indent=4)
